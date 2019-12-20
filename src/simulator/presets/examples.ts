c60e2980a368c8962584815e65f09ac511459b3b
yubrot <yubrot@gmail.com>
2019-12-21 00:25:07 +0900

    Improve simulator presets

--- src/simulator/presets/examples.ts
+++ src/simulator/presets/examples.ts
@@ -3,10 +3,10 @@ export const text =
 --- sphere
 hue <>
 rotate <> <> <>
-emit 20 1 1 {
+emit 16 1 1 {
   rotate 0 [] 0
-  emit 20 1 1 {
-    rotate [-90..90] 0 0
+  emit 16 1 1 {
+    rotate [-80..80] 0 0
     speed 3
     { 60 ease-out speed 0.1 | 20 nop; 50 opacity 0.1 }
     [0..40] nop
@@ -17,33 +17,69 @@ emit 20 1 1 {
   }
 }
 
---- spin
-hue 120
-44 emit 1 7 3 {
-  rotate 0 [] 0
-  hue+ 60
-  speed 2.13
-  { 86 ease-out speed 0.6 | 86 ease-in rotate 47 6 118 }
-  {
-    66 emit 1 8 1 {
-      rotate 0 180 0
-      hue+ 60
-      speed 1.16
-      46 nop
-      0 emit 2 1 1 {
-        rotate 0 [] 0
-        hue+ 60
-        speed 0.3
-        23 nop
-        { 45 ease-in speed* 4.27 | 45 ease-in rotate 0 36 8 }
-        52 close
-        120 nop
-        30 opacity 0
-      }
-    }
-  |
-    66 ease-in rotate 98 0 0
+--- rope
+hue 320
+32 emit 4 32 1 {
+  speed 2.5
+  rotate <> <> <>
+  { 50 ease-out speed 0.5 |
+    50 rotate <-40..40> <-40..40> 0 |
+    50 hue+ [40..80] }
+  emit 3 1 1 {
+    speed 0.5
+    { 100 ease-out speed 2.5 |
+      50 rotate <-40..40> <-40..40> 0 }
   }
 }
+
+--- fish
+hue 38
+rotate <> <> <>
+0 emit 3 1 1 {
+  rotate 0 [0..40] 0
+  hue+ 60
+  { speed 0.1; 58 nop |
+    57 ease-in rotate 17 5 14 }
+  40 emit 1 20 1 {
+    rotate 0 0 [0..330]
+    rotate 120 0 0
+    hue+ [60..100]
+    { speed 1.34; 33 nop |
+      46 ease-in rotate 14 5 22 }
+    0 emit 4 1 1 {
+      rotate 0 [41 .. 101] 0
+      hue+ 60
+      speed 1.06
+      70 ease-out rotate 40 61 0
+      50 close
+      120 nop
+      30 opacity 0
+    }
+  }
+}
+
+--- random-walk
+128 emit 1 128 1 {
+  hue []
+  speed 1
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+  speed 2
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+  speed 3
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+  speed 4
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+  speed 5
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+  speed 6
+  rotate <-90 0 90 180> <-90 0 90 180> 0
+  20 nop
+}
+
 ---
 `;
--- src/simulator/presets/reference.ts
+++ src/simulator/presets/reference.ts
@@ -14,13 +14,17 @@ close
 
 --- speed + *
 speed 1
+30 nop
 
-60 nop
+rotate 0 90 0
 speed 0.4
 60 nop
-speed+ -2
-60 nop
-120 speed* 0.1
+
+rotate 0 90 0
+60 speed+ 2
+
+rotate 0 90 0
+60 speed* 0.1
 
 --- opacity + *
 speed 0.5
@@ -34,7 +38,7 @@ speed 0.5
 speed 0.5
 
 180 hue 360
-speed* -1
+rotate 0 90 0
 180 hue+ -360
 
 --- translate
@@ -46,7 +50,7 @@ speed 0.2
 30 translate 0 30 0
 90 nop
 30 translate 0 0 30
-30 nop
+60 nop
 
 --- rotate
 speed 1
@@ -56,31 +60,31 @@ speed 1
 90 rotate 0 0 360
 
 --- emit
-60 emit 8 1 1 { speed 1; rotate 0 [] 0; 60 nop }
+60 emit 5 1 1 { speed 1; rotate 0 [] 0; 60 nop }
 60 nop
 hue+ 60
 60 emit 1 8 1 { speed 1; rotate 0 [] 0; 60 nop }
 60 nop
 hue+ 60
-60 emit 3 8 1 { speed 1; rotate 0 [] 0; 60 nop }
+60 emit 5 8 1 { speed 1; rotate 0 [] 0; 60 nop }
 60 nop
 hue+ 60
 60 emit 1 8 3 { speed 1; rotate 0 [] 0; 60 nop }
 60 nop
 hue+ 60
-60 emit 3 8 3 { speed 1; rotate 0 [] 0; 60 nop }
+60 emit 5 8 3 { speed 1; rotate 0 [] 0; 60 nop }
 120 nop
 hue+ 60
-60 emit 3 1 3 { speed 1; rotate 0 [] 0; 60 nop }
+60 emit 5 1 3 { speed 1; rotate 0 [] 0; 60 nop }
 
 --- loop
 speed 1
 
 400 loop {
-  hue+ 60
-  rotate 0 45 0
+  hue+ 10
+  rotate 0 15 0
   emit 1 1 1 { 90 nop }
-  30 nop
+  10 nop
 }
 
 --- repeat
@@ -95,55 +99,52 @@ repeat 12 {
 --- block
 speed 1
 
-repeat 4 {
-  60 emit 1 9 1 { rotate 0 180 0; 60 nop }
+{
+  360 emit 1 36 1 { rotate 0 180 0; 60 nop }
 |
-  60 rotate 0 90 0
+  180 rotate 0 360 0
+  180 rotate 0 -360 0
 |
-  60 hue+ 90
+  360 hue+ 360
 }
 
 --- (easing)
 emit 4 1 1 {
-  rotate 0 [] 0
-  speed 0
-  100 [linear ease-in ease-out ease-in-out] speed 2
+  translate [-20..20] 0 0
+  [40..200] nop
+  speed 2
+  100 [linear ease-in ease-out ease-in-out] speed 0
 }
 
 --- $each-choice
-speed 1
-
-emit 16 1 1 {
-  rotate 0 [] 0;
-  60 nop
-  60 rotate 0 [90 -90] 0
+128 emit 1 24 1 {
+  speed 1
+  30 nop
+  30 rotate 0 [-60 -20 20 60] 0
   60 nop
 }
 
 --- $each-range
-speed 1
-
-60 emit 10 1 1 { rotate 0 [-30..30] 0; 60 nop }
-speed* -1
-60 emit 10 1 1 { rotate 0 [-60..60] 0; 60 nop }
-speed* -1
-60 emit 10 1 1 { rotate 0 [-90..90] 0; 60 nop }
+60 emit 10 1 1 { speed 1; rotate 0 [-30..30] 0; 60 nop }
+translate 0 10 0
+60 emit 10 1 1 { speed 1; rotate 0 [-60..60] 0; 60 nop }
+translate 0 10 0
+60 emit 10 1 1 { speed 1; rotate 0 [-90..90] 0; 60 nop }
 
 --- $each-angle
-speed 1
-
-60 emit 8 1 1 { rotate 0 [] 0; hue []; 60 nop }
-speed* -1
-60 emit 16 1 1 { rotate 0 [] 0; hue []; 60 nop }
-speed* -1
-60 emit 32 1 1 { rotate 0 [] 0; hue []; 60 nop }
+60 emit 16 1 1 { speed 0.7; rotate 0 [] 0; hue []; 180 nop }
+60 emit 32 1 1 { speed 1; rotate 0 [] 0; hue []; 120 nop }
+60 emit 64 1 1 { speed 1.5; rotate 0 [] 0; hue []; 60 nop }
 
 --- $random-choice
-emit 8 1 1 {
-  speed <1 2 3>
-  rotate 0 [] 0
-  60 nop
-}
+speed 3
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
+rotate <-90 0 90> <-90 0 90> 0; 10 nop
 
 --- $random-range
 32 emit 1 32 1 {
